f170e70f8a722e253ec9644626dfacc381b0c852
Nikhil Gohil <nikhilgohil@Nikhils-MacBook-Pro.local>
2017-01-25 17:24:38 +0530

    Contentstack swift playground Initial Commit

---
+++ ContentstackPlayground.playground/Sources/CSExtensions.swift
@@ -0,0 +1,35 @@
+import Foundation
+import UIKit
+
+public extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
+
+//import Foundation
+//
+//
+//let AccessToken = "blt0c4300391e033d4a59eb2857"
+//let APIKEY = "blt920bb7e90248f607"
+//let EnvironmentName = "production"
+//
+//public class Contentstack: NSObject {
+//    
+//    
+//}
